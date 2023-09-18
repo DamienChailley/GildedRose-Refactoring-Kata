4067a10d9263b0092266ab5dfe8e77845becd3f7
DamienChailley <playwithcube@gmail.com>
2023-09-18 20:55:27 +0200

    Add Nunit framework + improve console log :)

---
+++ csharpcore/GildedRose/ConsoleTabHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseKata
+{
+    internal static class ConsoleTabHelper
+    {
+        public static string getPadRightString(string str, int nbSpace = 42)
+        {
+            return str.PadRight(nbSpace, ' ');
+        }
+    }
+}
---
+++ csharpcore/GuildRoseUnitTests/ApprovalTest.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using System.Text;
+using GildedRoseKata;
+using NUnit.Framework;
+
+namespace GildedRoseTests;
+
+public class ApprovalTest
+{
+    [Test]
+    public void TestThirtyDays()
+    {
+        Assert.Pass();
+    }
+}
